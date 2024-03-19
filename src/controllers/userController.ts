826396778970b07f7c882899bab2e83d78444eaa
Pedro Germano <pedrogermano232@gmail.com>
2024-03-19 10:20:32 -0300

    FEAT: create userEmailAlreadyExists

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,11 +5,63 @@ export const listUsersController = async (
   request: Request,
   response: Response
 ) => {
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
+  const users = await prisma.user.findMany();
+  response.send(users);
+};
+
+export const findOneUserController = async (
+  request: Request,
+  response: Response
+) => {
+  const { userId } = request.params;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
     },
   });
 
-  response.send(users);
+  if (!user) {
+    return response.status(404).send({
+      error: "Not Found",
+    });
+  }
+
+  response.send(user);
+};
+
+export const createUserController = async (
+  request: Request,
+  response: Response
+) => {
+  const { name, email } = request.body;
+
+  if(!name || !email){
+    return response.send({
+      error: 'Name or email is invalid'
+    })
+  }
+
+  const userEmailAlreadyExists = await prisma.user.findUnique({
+    where: {
+      email
+    },
+    select: {
+      id: true
+    }
+  });
+
+  if (userEmailAlreadyExists) {
+    return response.status(400).send({
+      error: "Email already in use",
+    });
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email
+    }
+  })
+
+  response.send(user);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { listUsersController } from "./controllers/userController";
+import { findOneUserController, listUsersController } from "./controllers/userController";
 
 const app = express();
 const port = 3000;
 
 app.get("/users", listUsersController);
+app.get("/users/:userId", findOneUserController)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
